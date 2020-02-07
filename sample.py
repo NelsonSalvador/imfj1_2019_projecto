39ade081d1916115fa9cdb1708e3ca267ca4660a
Nelson Salvador <nelsonsalvadorzx@gmail.com>
2020-02-07 14:51:48 -0500

    Adicionei mais objetos, tentei implementar o back face culling mas deixei em comentario porque nao estava a funcionar

--- sample.py
+++ sample.py
@@ -45,12 +45,22 @@ def main():
 
     # Create a cube and place it in a scene, at position (0,0,0)
     # This cube has 1 unit of side, and is red
-    obj1 = Object3d("TestObject")
-    obj1.scale = vector3(1, 1, 1)
-    obj1.position = vector3(0, -1, 0)
-    obj1.mesh = Mesh.create_cube((1, 1, 1))
-    obj1.material = Material(color(1,0,0,1), "TestMaterial1")
-    scene.add_object(obj1)
+    
+
+    obj3 = Object3d("TestObject1")
+    obj3.position = vector3(2, 0, 2)
+    obj3.mesh = Mesh.create_cube((1.5, 1, 1.5))
+    obj3.material = Material(color(1,0,0,1), "TestMaterial3")
+
+    obj4 = Object3d("TestObject2")
+    obj4.position = vector3(0, 0, 3)
+    obj4.mesh = Mesh.create_cube((1.5, 1.5, 1))
+    obj4.material = Material(color(1,0,0,1), "TestMaterial4")
+
+    obj5 = Object3d("TestObject3")
+    obj5.position = vector3(3, 0, 0)
+    obj5.mesh = Mesh.create_cube((0.25, 0.25, 0.25))
+    obj5.material = Material(color(1,0,0,1), "TestMaterial4")
 
     # Create a second object, and add it as a child of the first object
     # When the first object rotates, this one will also mimic the transform
@@ -58,13 +68,27 @@ def main():
     obj2.position += vector3(0, 0.75, 0)
     obj2.mesh = Mesh.create_cube((0.5, 0.5, 0.5))
     obj2.material = Material(color(0,1,0,1), "TestMaterial2")
+
+    obj1 = Object3d("TestObject")
+    obj1.scale = vector3(1, 1, 1)
+    obj1.position = vector3(0, -1, 0)
+    obj1.mesh = Mesh.create_cube((1, 1, 1))
+    obj1.material = Material(color(1,0,0,1), "TestMaterial1")
+
+    scene.add_object(obj1)
     obj1.add_child(obj2)
+    obj1.add_child(obj3)
+    obj1.add_child(obj4)
+    obj1.add_child(obj5)
+    
+    
 
     # Specify the rotation of the object. It will rotate 15 degrees around the axis given, 
     # every second
     angle = 0
     axis = vector3(1,0.7,0.2)
     axis.normalize()
+
 
     # Timer
     delta_time = 0
@@ -73,10 +97,12 @@ def main():
     pygame.mouse.set_visible(False)
     pygame.event.set_grab(True)
 
+
     # Game loop, runs forever
     while (True):
         # Process OS events
         for event in pygame.event.get():
+            scene.add_object(obj1)
             # Checks if the user closed the window
             if (event.type == pygame.QUIT):
                 # Exits the application immediately
@@ -165,22 +191,34 @@ def main():
             q = from_rotation_vector((axis * math.radians(angle) * delta_time).to_np3())
             obj1.rotation = q * obj1.rotation
         if kq:
-            scene.camera.position -= vector3(0,0,0.001)
+            scene.camera.position -= vector3(0,0,0.1)
         if ke:
-            scene.camera.position += vector3(0,0,0.001)
+            scene.camera.position += vector3(0,0,0.1)
         if ka:
-            scene.camera.position += vector3(0.001,0,0)
+            scene.camera.position += vector3(0.1,0,0)
         if kd:
-            scene.camera.position -= vector3(0.001,0,0)
+            scene.camera.position -= vector3(0.1,0,0)
         if kw:
-            scene.camera.position -= vector3(0,0.001,0)
+            scene.camera.position -= vector3(0,0.1,0)
         if ks:
-            scene.camera.position += vector3(0,0.001,0)
+            scene.camera.position += vector3(0,0.1,0)
+        
+        #Back Face culling
+        #for i in obj1.mesh.polygons:
+         #   n = cross_product(i[1], i[2])
+          #  v =  scene.camera.position
+           # if dot_product(v, n) < 0:
+            #    obj1.mesh.polygons.append(i)
+           # else:
+            #    obj1.mesh.polygons.remove(i)
+        #Mesh.render(screen, obj1.mesh.polygons, Material(color(1,0,0,1)))
+
 
         # Clears the screen with a very dark blue (0, 0, 20)
         screen.fill((0,0,20))
 
         scene.render(screen)
+        
 
         # Swaps the back and front buffer, effectively displaying what we rendered
         pygame.display.flip()
